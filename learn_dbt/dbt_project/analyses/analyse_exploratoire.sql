955e892f5d08bd816dafc37e6bdb9944c6fdaeb3
Jeremie <jeremienodzo@gmail.com>
2026-05-29 09:39:37 +0200

    First exploratory SQL queries for yellow taxi trip data analysis

---
+++ learn_dbt/dbt_project/analyses/analyse_exploratoire.sql
@@ -0,0 +1,8 @@
+SELECT 
+    * 
+FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet' 
+LIMIT 10;
+
+SELECT 
+    COUNT(*) 
+FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet' 
