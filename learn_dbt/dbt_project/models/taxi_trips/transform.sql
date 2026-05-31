534b03682589073f8bd8092227e7f2f64e018e30
Jeremie <jeremienodzo@gmail.com>
2026-05-31 17:46:16 +0200

    Add data dictionary for yellow taxi trip records, update .gitignore, modify sources.yml for external location, enhance transform.sql for 2024 data filtering, and add test for distinct months

--- learn_dbt/dbt_project/models/taxi_trips/transform.sql
+++ learn_dbt/dbt_project/models/taxi_trips/transform.sql
@@ -1,29 +1,36 @@
+-- Config for materialization as an external table in Parquet format
+{{ config(
+    materialized = 'external',
+    location = 'output/trips_2024_transformed.parquet',
+    file_format = 'parquet'
+) }} 
 -- Test data source from yml
 -- SELECT  tpep_pickup_datetime, tpep_dropoff_datetime, trip_distance, passenger_count, total_amount
 -- FROM {{  source('tlc_taxi_trips', 'raw_yellow_tripdata') }}
 -- LIMIT 10
-
 -- Define CTEs to filter out invalid trips
-
 -- Define data source
 WITH source_data AS (
-    SELECT * EXCLUDE (VendorID, RateCodeID)
-    FROM {{ source('tlc_taxi_trips', 'raw_yellow_tripdata') }}
+    SELECT
+        * EXCLUDE (VendorID, RateCodeID)
+    FROM
+        {{ source('tlc_taxi_trips', 'raw_yellow_tripdata') }}
 ),
-
 -- Apply filters to clean the data
 filtered_data AS (
-    SELECT *
-    FROM source_data
-    WHERE passenger_count > 0 -- Exclude trips with zero passengers
-      AND trip_distance > 0 -- Exclude trips with zero or negative distance
-      AND total_amount > 0 -- Exclude trips with zero or negative total amount
-      AND tpep_pickup_datetime < tpep_dropoff_datetime -- Exclude trips with negative duration
-      AND store_and_fwd_flag = 'N' -- Exclude trips that were stored and forwarded
-      AND tip_amount >= 0 -- Exclude trips with negative tip amount
-      AND payment_type IN (1, 2) -- Exclude trips with invalid payment types
+    SELECT
+        *
+    FROM
+        source_data
+    WHERE
+        passenger_count > 0 -- Exclude trips with zero passengers
+        AND trip_distance > 0 -- Exclude trips with zero or negative distance
+        AND total_amount > 0 -- Exclude trips with zero or negative total amount
+        AND tpep_pickup_datetime < tpep_dropoff_datetime -- Exclude trips with negative duration
+        AND store_and_fwd_flag = 'N' -- Exclude trips that were stored and forwarded
+        AND tip_amount >= 0 -- Exclude trips with negative tip amount
+        AND payment_type IN (1, 2) -- Exclude trips with invalid payment types
 ),
-
 -- Final transformed data
 transformed_data AS (
     SELECT
@@ -35,13 +42,33 @@ transformed_data AS (
             WHEN payment_type = 2 THEN 'Cash'
         END AS payment_method,
         -- Feature engineering: Calculate trip duration in minutes
-        DATE_DIFF('minute', tpep_pickup_datetime, tpep_dropoff_datetime) AS trip_duration_minutes,
+        DATE_DIFF(
+            'minute',
+            tpep_pickup_datetime,
+            tpep_dropoff_datetime
+        ) AS trip_duration_minutes,
         -- Select other relevant columns
         * EXCLUDE (passenger_count, payment_type)
-    FROM filtered_data
-)
-
--- Select final transformed data
-SELECT *
-FROM transformed_data
-WHERE trip_duration_minutes > 0
+    FROM
+        filtered_data
+),
+-- Filter for trips in the year 2024
+final_data AS (
+    SELECT
+        *,
+        CAST(tpep_pickup_datetime AS DATE) AS pickup_date,
+        CAST(tpep_dropoff_datetime AS DATE) AS dropoff_date
+    FROM
+        transformed_data
+    WhERE
+        pickup_date >= '2024-01-01'
+        AND pickup_date < '2025-01-01' -- Filter for trips in the year 2024
+        AND dropoff_date >= '2024-01-01'
+        AND dropoff_date < '2025-01-01' -- Filter for trips in the year 2024
+) -- Select final transformed data
+SELECT
+    * EXCLUDE (pickup_date, dropoff_date)
+FROM
+    final_data
+WHERE
+    trip_duration_minutes > 0
